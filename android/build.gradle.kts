4c889a577fd41c8a9872acaac93710016a099ad1
vikricahya64-alt <vikricahya64@gmail.com>
2026-08-21 14:36:07 +0700

    test: add ktlint 12.2.0 plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     id("com.android.application") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.2.0" apply false
 }
